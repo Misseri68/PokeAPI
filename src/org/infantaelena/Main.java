d654f98ff7bbbdce26cfb621025d4f3d3655c135
adriana <adriaranguez89@gmail.com>
2023-05-20 19:30:06 +0200

    pokeView parte III

--- src/org/infantaelena/Main.java
+++ src/org/infantaelena/Main.java
@@ -1,14 +1,25 @@
 package org.infantaelena;
 
 import org.infantaelena.controlador.Controlador;
+import org.infantaelena.vista.PokemonView;
+import org.infantaelena.vista.Vista;
+
+import javax.swing.*;
 
 public class Main
 
 {
 
     public static void main(String args[]) {
-
-        new Controlador();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new PokemonView();
+                frame.setSize(300, 300);
+                frame.setVisible(true);
+            }
+        });
+  
 
     }
 

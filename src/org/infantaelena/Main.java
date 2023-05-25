032f3bd2bc4d10251867f9b18f63b0d98c176f51
Misseri <ariadna68.ariadna@gmail.com>
2023-05-25 19:21:14 +0200

    fix del form para borrar pokemon, de las excepciones de modelo hacia controlador, existePokemon ahora usa nombrePokenon en vez de Pokemon, ahora se inicializa y se usan getters del form de eliminarpokemon solo dentro de su form inicializado, y más..

--- src/org/infantaelena/Main.java
+++ src/org/infantaelena/Main.java
@@ -5,8 +5,6 @@ import org.infantaelena.controlador.Controlador;
 
 public class Main {
     public static void main(String args[]) {
-//PreguntarparaEliminar p = new PreguntarparaEliminar();
-
     Controlador controlador = new Controlador();
 
     }

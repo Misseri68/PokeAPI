f281cf7b14967922b7a3be6296c3bd0115f5d955
MasterixGMR <Dragos.diinu@gmail.com>
2023-05-25 14:02:01 +0200

    implementacion de borrar en controlador, intento de arreglar Confirmacion, pero anza nullpointer exception
    Exception in thread "main" java.lang.NullPointerException
    	at org.infantaelena.vista.Confirmacion.$$$setupUI$$$(Confirmacion.java)
    	at org.infantaelena.vista.Confirmacion.<init>(Confirmacion.java:14)
    	at org.infantaelena.Main.main(Main.java:9)

--- src/org/infantaelena/vista/Confirmacion.java
+++ src/org/infantaelena/vista/Confirmacion.java
@@ -1,5 +1,82 @@
 package org.infantaelena.vista;
 
+import javax.swing.*;
+
+public class Confirmacion extends JFrame {
+    private JPanel confirmacionJPanel;
+    private JButton sibutton;
+    private JButton noButton;
+    private JLabel eliminarJLabel;
+    private JLabel preguntaJlabel;
+
+    public Confirmacion( ) {
+        super("pokeAPI");
+        confirmacionJPanel = new JPanel();  // Inicializo el JPanel
+        setContentPane(confirmacionJPanel);
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        // Initialize the JButton components
+        sibutton = new JButton("Sí");
+        noButton = new JButton("No");
+
+        // Initialize the JLabel components
+        eliminarJLabel = new JLabel("Vas a eliminar el pokemon,");
+        preguntaJlabel = new JLabel("¿Estás seguro?");
+
+        confirmacionJPanel.add(eliminarJLabel);
+        confirmacionJPanel.add(preguntaJlabel);
+        confirmacionJPanel.add(sibutton);
+        confirmacionJPanel.add(noButton);
+    }
+    public JPanel getConfirmacionJPanel() {
+        return confirmacionJPanel;
+    }
+
+    public void setConfirmacionJPanel(JPanel confirmacionJPanel) {
+        this.confirmacionJPanel = confirmacionJPanel;
+    }
+
+    public JButton getSibutton() {
+        return sibutton;
+    }
+
+    public void setSibutton(JButton sibutton) {
+        this.sibutton = sibutton;
+    }
+
+    public JButton getNoButton() {
+        return noButton;
+    }
+
+    public void setNoButton(JButton noButton) {
+        this.noButton = noButton;
+    }
+
+    public JLabel getEliminarJLabel() {
+        return eliminarJLabel;
+    }
+
+    public void setEliminarJLabel(JLabel eliminarJLabel) {
+        this.eliminarJLabel = eliminarJLabel;
+    }
+
+    public JLabel getPreguntaJlabel() {
+        return preguntaJlabel;
+    }
+
+    public void setPreguntaJlabel(JLabel preguntaJlabel) {
+        this.preguntaJlabel = preguntaJlabel;
+    }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
+package org.infantaelena.vista;
+
 import org.infantaelena.modelo.entidades.Tipo;
 
 import javax.swing.*;
